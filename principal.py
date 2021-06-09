3a801d99be14f6d4f67c9c562fe2f918ebba8fcf
SalimVazquez <181086@ids.upchiapas.edu.mx>
2021-06-08 21:38:44 -0500

    Initial form

--- principal.py
+++ principal.py
@@ -5,14 +5,36 @@ import random
 
 # globals 
 root = Tk()
+fields = (
+    'Lambda',
+    'Error permisible',
+)
+
+def start(entries):
+    print('Initializing...')
+
+def makeform(root, fields):
+    title = Label(root, text="Inicialización", width=20, font=("bold",20))
+    title.pack()
+    entries = {}
+    for field in fields:
+        row = Frame(root)
+        lab = Label(row, width=30, text=field+": ", anchor='w')
+        ent = Entry(row)
+        row.pack(side=TOP, fill=X, padx=5, pady=5)
+        lab.pack(side=LEFT)
+        ent.pack(side=RIGHT, expand=YES, fill=X)
+        entries[field] = ent
+    return entries
 
 if __name__ == '__main__':
     root.title("Entrenamiento Neurona - UPCH IA")
     root.geometry("300x250")
     root.resizable(0,0)
-    title = Label(root, text="Inicialización", width=20, font=("bold",20))
-    title.pack()
-    b1 = Button(root, text = 'Iniciar', bg="green",fg='white')
+    ents = makeform(root, fields)
+    root.bind('<Return>', (lambda event, e=ents:fetch(e)))
+    b1 = Button(root, text = 'Iniciar',
+        command=(lambda e=ents: start(e)), bg="green",fg='white')
     b1.pack(side = LEFT, padx = 5, pady = 5, expand = YES)
     b2 = Button(root, text = 'Quit', command = root.quit, bg="red",fg='white')
     b2.pack(side = LEFT, padx = 5, pady = 5, expand = YES)
