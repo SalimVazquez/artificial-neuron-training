81db854df4aa13cd6c94787b2b39677a816772c4
SalimVazquez <181086@ids.upchiapas.edu.mx>
2021-07-07 13:14:17 -0500

    D2L implemented and integrated

--- FAEscalon.py
+++ FAEscalon.py
@@ -1,9 +1,18 @@
-from mxnet import gluon, nd
+from mxnet import gluon, np, npx, nd
 from mxnet.gluon import nn
+
+npx.set_np()
 
 class FAEscalon(nn.Block):
 	def __init__(self, **kwargs):
 		super(FAEscalon, self).__init__(**kwargs)
 
-	def forward(self, x):
-		return 0 if x <= 0 else 1
+	def forward(self, data):
+		aux = []
+		i = 0
+		for x in data:
+			print(f'x{i+1}->',x)
+			aux.append(0 if x < 1 else 1)
+			i += 1
+		print(aux)
+		return nd.array(aux)
--- principal.py
+++ principal.py
@@ -1,119 +1,143 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 from tkinter import *
 from tkinter import messagebox
-import numpy as np
-import random
-import math
+# Library for graph
 import matplotlib.pyplot as plot
+# Library of D2L
+from d2l import mxnet as d2l
+from mxnet import autograd, np, npx, gluon, init, nd
+from mxnet.gluon import nn
+import pandas as pd
+# Class for Activation Function
+from FAEscalon import FAEscalon
 
-# globals 
+# globals
+npx.set_np()
+# Architecture of networks
+# with one output and FA "relu"
+net = nn.Sequential()
+net.add(nn.Dense(1, activation='relu'))
+# net.add(nn.Dense(1), FAEscalon())
+# normal distribution in weights (sigma)
+net.initialize(init.Normal(sigma=0.03))
 root = Tk()
-fields = (
-    'Lambda',
-    'Error permisible',
-)
+fields = ('Lambda', 'Error permisible')
+loss = gluon.loss.L2Loss()
 
-def FAEscalon(U):
-    Yc = []
-    for i in range(len(U)):
-        if U[i] <= 0:
-            Yc.append(0)
-        else:
-            Yc.append(1)
-    return np.array(Yc)
-
-def calculateError(E):
-    result = 0
-    for i in range(len(E)):
-        result = result + math.pow(E[i], 2)
-    return math.sqrt(result)
-
-def ReadFile():
-    f = open('data.txt', 'r')
-    data = f.read()
-    clean = data.rsplit("\n")
-    X = np.matrix(clean[0]) # create matrix X
-    y = clean[1].replace(";", ",")
-    b = []
-    for i in range(0, len(y), 2):
-        b.append(y[i])
-    Yaux = [int(e) for e in b]
-    Y = np.array(Yaux) # create array Y
-    f.close()
-    return X, Y
-
-def graphEvol(x,y, lamb):
-    plot.xlabel('Epocas')
-    plot.ylabel('Norma del error')
-    plot.title('Evolución de la norma del error')
-    plot.plot(x, y, markerfacecolor='blue',
-             markersize=6, color='skyblue', linewidth=3, label='Lambda: '+str(lamb))
-    plot.legend(bbox_to_anchor=(1, 1), loc='upper left', borderaxespad=0.)
+def graphEvol(x,y,lamb,w):
+    (fig, axs) = plot.subplots(2, 1)
+    labelCol = 'Weights'
+    axs[0].axis('tight')
+    axs[0].axis('off')
+    axs[0].set_title('Weights finals')
+    columns = ['W {:X}'.format(i + 1) for i in range(len(w[0]))]
+    rows = ['Weights' for i in range(1)]
+    data = [['{:02}'.format(w[r][c]) for c in range(len(w[0]))]
+            for r in range(1)]
+    table = axs[0].table(cellText=data, rowLabels=rows,
+                         colLabels=columns, cellLoc='center',
+                         loc='center')
+    table.set_fontsize(16)
+    table.scale(1.2, 1.1)
+    plot.xlabel('Epochs')
+    plot.ylabel('|E|')
+    axs[1].set_title('Evolution of the error norm')
+    axs[1].plot(x,y,
+        markerfacecolor='blue',
+        markersize=6,
+        color='skyblue',
+        linewidth=3,
+        label='Lambda: ' + str(lamb),
+        )
+    plot.legend(bbox_to_anchor=(1, 1), loc='upper center',
+                borderaxespad=0.)
     plot.show()
 
-def start(entries):
+def load_array(data_arrays, batch_size, is_train=True):
+    dataset = gluon.data.ArrayDataset(*data_arrays)
+    return gluon.data.DataLoader(dataset, batch_size, shuffle=is_train)
+
+def divideArray(l, n):
+    for i in range(0, len(l), n):
+        yield l[i:i + n]
+
+def ReadFile():
+    # get data from data.txt
+    data = pd.read_table('data.txt', delimiter='\t', header=None)
+    (X, Y) = (data.iloc[0, :], data.iloc[1, :])
+    # cleaning matriz X
+    a = X[0].split(';')
+    b = []
+    # convert string to float
+    for i in range(len(a)):
+        for j in range(0, len(a[i]), 2):
+            b.append(float(a[i][j]))
+    # Split list to array, for create matriz X
+    X = list(divideArray(b, 3))
+    X = np.array(X)
+    # cleaning array Y
+    a = Y[0].split(';')
+    b = []
+    # convert string to float
+    for i in range(len(a)):
+        b.append(float(a[i]))
+    # create array Y
+    Y = np.array(b)
+    return (X, Y)
+
+def start(p):
     epochs = []
-    evolNorm = []
+    evol_norm = []
     i = 0
-    X, Y = ReadFile()
-    lamb = float(entries['Lambda'].get())
-    eps = float(entries['Error permisible'].get())
-    if lamb > 0 and lamb <= 1:
-        print('X:\n', X)
-        print('Y: ', Y)
-        print('Lambda: ', lamb)
-        print('Error: ', eps)
-        dimensionsX = X.shape
-        print('dimensionsX: ', dimensionsX)
-        # adding bias
-        bias = [1 for i in range(dimensionsX[0])]
-        X = np.insert(X, 0, bias, axis=1)
-        dimensionsX = X.shape
-        print('bias added\ndimensionsX: ', dimensionsX)
-        m = dimensionsX[0]
-        n = dimensionsX[1]
+    lr = float(p['Lambda'].get())
+    eps = float(p['Error permisible'].get())
+    if lr > 0 and lr <= 1:
+        (X, Y) = ReadFile()
+        print ('matriz X->\n', X, '\nDimention:', X.shape)
+        print ('array Y->', Y, '\nDimention:', Y.shape)
+        print ('Lambda: ', lr)
+        print ('Error perm: ', eps)
+        (m, n) = (X.shape[0], X.shape[1])
         if n > 1 or m >= 2:
-            W = np.random.rand(n,1)
+            data_iter = load_array((X, Y), m)
+            trainer = gluon.Trainer(net.collect_params(), 'sgd',
+                                    {'learning_rate': lr})
             while True:
-                print('<----- Epoca #', i+1, ' ----->')
+                print ('<---Epoch #', i + 1, '--->')
                 epochs.append(i)
-                print('W:\n', W)
-                U = X.dot(W)
-                print('U:\n', U)
-                Yc = FAEscalon(U)
-                print('Yc:', Yc)
-                E = Yc - Y
-                print('E:', E)
-                EtX = np.dot(E.transpose(), X)
-                print('Et * X: ',EtX)
-                Ne = lamb * EtX
-                print('n * Et * X: ', Ne)
-                W = W.transpose() - Ne
-                print('new W: ',W)
-                enorm = calculateError(E)
-                print('ENorm:', enorm)
-                evolNorm.append(enorm)
-                if enorm > eps:
-                    W = W.transpose()
-                    print('Try again!')
+                for (x, y) in data_iter:
+                    with autograd.record():
+                        l = loss(net(x), y)
+                    l.backward()
+                    trainer.step(m)
+                l = loss(net(X), Y)
+                print(f'epoch {i+1}, loss {l.mean().asnumpy():f}')
+                print(f'weights {net[0].weight.data()}')
+                evol_norm.append(l.mean().asnumpy())
+                if l.mean().asnumpy() > eps:
+                    print('Try again')
                     i += 1
                 else:
                     break
-            print('Finish')
-            messagebox.showinfo("Norma del error", str(enorm))
-            messagebox.showinfo("Configuración W", str(W))
-            graphEvol(epochs, evolNorm, lamb)
+            w = net[0].weight.data()
+            graphEvol(epochs, evol_norm, lr, w)
         else:
-            messagebox.showerror("Parametros incorrectos", "Dimensiones no correctas")
+            messagebox.showerror('Parametros incorrectos',
+                                 'Dimensiones no correctas')
     else:
-        messagebox.showerror("Parametros incorrectos", "Lambda fuera de parametros (0, 1]")
+        messagebox.showerror('Parametros incorrectos',
+                             'Lambda fuera de parametros (0, 1]')
+
 
 def makeform(root, fields):
-    title = Label(root, text="Inicialización", width=20, font=("bold",20))
+    title = Label(root, text='Inicializacion', width=20,
+                  font=('bold', 20))
     title.pack()
     entries = {}
     for field in fields:
         row = Frame(root)
-        lab = Label(row, width=30, text=field+": ", anchor='w')
+        lab = Label(row, width=30, text=field + ': ', anchor='w')
         ent = Entry(row)
         row.pack(side=TOP, fill=X, padx=5, pady=5)
         lab.pack(side=LEFT)
@@ -122,14 +146,15 @@ def makeform(root, fields):
     return entries
 
 if __name__ == '__main__':
-    root.title("Entrenamiento Neurona - UPCH IA")
-    root.geometry("300x250")
-    root.resizable(0,0)
+    root.title('Entrenamiento Neurona - UPCH IA')
+    root.geometry('300x250')
+    root.resizable(0, 0)
     ents = makeform(root, fields)
-    root.bind('<Return>', (lambda event, e=ents:fetch(e)))
-    b1 = Button(root, text = 'Iniciar',
-        command=(lambda e=ents: start(e)), bg="green",fg='white')
-    b1.pack(side = LEFT, padx = 5, pady = 5, expand = YES)
-    b2 = Button(root, text = 'Quit', command = root.quit, bg="red",fg='white')
-    b2.pack(side = LEFT, padx = 5, pady = 5, expand = YES)
+    root.bind('<Return>', lambda event, e=ents: fetch(e))
+    b1 = Button(root, text='Iniciar', command=lambda e=ents: start(e),
+                bg='green', fg='white')
+    b1.pack(side=LEFT, padx=5, pady=5, expand=YES)
+    b2 = Button(root, text='Quit', command=root.quit, bg='red',
+                fg='white')
+    b2.pack(side=LEFT, padx=5, pady=5, expand=YES)
     root.mainloop()
